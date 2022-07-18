8b631fd3c8c720c42c8aeb917ad5aad38f4c6d52
Nivetha <nivetha.s1701@gmail.com>
2022-07-18 13:06:52 +0530

    Update
    Update

--- precache-manifest.8997140856635e11c813839b219b82dc.js
+++ precache-manifest.8997140856635e11c813839b219b82dc.js
@@ -1,26 +1,26 @@
 self.__precacheManifest = [
   {
     "revision": "22e660ce69f7e2ad833f",
-    "url": "/social-network/static/js/runtime~main.22e660ce.js"
+    "url": "/socio/static/js/runtime~main.22e660ce.js"
   },
   {
     "revision": "751460f4967145dabf17",
-    "url": "/social-network/static/js/main.751460f4.chunk.js"
+    "url": "/socio/static/js/main.751460f4.chunk.js"
   },
   {
     "revision": "dbe6aaa09972b444c1c7",
-    "url": "/social-network/static/js/1.dbe6aaa0.chunk.js"
+    "url": "/socio/static/js/1.dbe6aaa0.chunk.js"
   },
   {
     "revision": "751460f4967145dabf17",
-    "url": "/social-network/static/css/main.79d6dbc2.chunk.css"
+    "url": "/socio/static/css/main.79d6dbc2.chunk.css"
   },
   {
     "revision": "dbe6aaa09972b444c1c7",
-    "url": "/social-network/static/css/1.fe5b4aea.chunk.css"
+    "url": "/socio/static/css/1.fe5b4aea.chunk.css"
   },
   {
     "revision": "14ee7cd3d1f6afd476d41c4f3ebe6c36",
-    "url": "/social-network/index.html"
+    "url": "/socio/index.html"
   }
 ];
